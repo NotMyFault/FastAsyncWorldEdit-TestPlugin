0c43bf0ef2b1993bfc9f87be628b006911177379
NotMyFault <mc.cache@web.de>
2021-10-16 21:48:51 +0200

    Scan builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "FastAsyncWorldEditTest"
+
+
+plugins {
+
+    id("com.gradle.enterprise") version("3.7")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
