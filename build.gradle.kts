9ae4d14aa0a0b6b714866708410e1307531000c7
Alex <mc.cache@web.de>
2022-02-13 22:19:17 +0100

    build: Update Fawe

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,27 +13,19 @@ repositories {
         name = "PaperMC"
         url = uri("https://papermc.io/repo/repository/maven-public/")
     }
-    maven {
-        name = "IntellectualSites Releases"
-        url = uri("https://mvn.intellectualsites.com/content/repositories/releases/")
-    }
-    maven {
-        name = "IntellectualSites Snapshots"
-        url = uri("https://mvn.intellectualsites.com/content/repositories/snapshots/")
-    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:1.17-308")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.0.1")
 }
 
 the<JavaPluginExtension>().toolchain {
-    languageVersion.set(JavaLanguageVersion.of(16))
+    languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 configurations.all {
-    attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 16)
+    attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
 tasks.compileJava.configure {
